1951c085189b51d4c4bfd11417ac85d2958fee12
kitplummer <kit.plummer@gtri.gatech.edu>
2020-01-02 17:25:08 -0500

    update: bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule LowendinsightGet.MixProject do
   def project do
     [
       app: :lowendinsight_get,
-      version: "0.1.1",
+      version: "0.1.2",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       deps: deps()

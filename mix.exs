718be0c8cf9b847833f4cd36211c76d9b2c6f8d4
kitplummer <kit.plummer@gtri.gatech.edu>
2020-01-23 15:27:16 -0500

    update version bump to matchy-match

--- mix.exs
+++ mix.exs
@@ -8,7 +8,7 @@ defmodule LowendinsightGet.MixProject do
   def project do
     [
       app: :lowendinsight_get,
-      version: "0.3.0",
+      version: "0.3.1",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       deps: deps()

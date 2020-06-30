9eb21360a5128b0560379a3461c493b900c302db
kitplummer <kit.plummer@gtri.gatech.edu>
2020-06-30 16:36:05 -0400

    fix: bump version to match lei at 0.6.1

--- mix.exs
+++ mix.exs
@@ -8,7 +8,7 @@ defmodule LowendinsightGet.MixProject do
   def project do
     [
       app: :lowendinsight_get,
-      version: "0.5.0",
+      version: "0.6.1",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       deps: deps(),

305412ffae212055886a23e0ffe0a999ad1c8822
kitplummer <kit.plummer@gtri.gatech.edu>
2020-07-03 10:15:23 -0400

    fix: move to main from master, version bump

--- mix.exs
+++ mix.exs
@@ -8,7 +8,7 @@ defmodule LowendinsightGet.MixProject do
   def project do
     [
       app: :lowendinsight_get,
-      version: "0.6.1",
+      version: "0.6.2",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       deps: deps(),

06a0423801248149244ecc14087b67608c145c8b
haldihri3 <houida.aldihri@gtri.gatech.edu>
2020-07-15 15:17:55 -0400

    update: scripts runs process_languages

--- scripts/trending.sh
+++ scripts/trending.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-allLanguages=("elixir" "python" "java" "javascript" "rust" "go" "ruby" "c" "c++" "c#" "haskell" "php" "scala" "swift" "objective-c" "kotlin" "shell" "typescript")
+# allLanguages=("elixir" "python" "java" "javascript" "rust" "go" "ruby" "c" "c++" "c#" "haskell" "php" "scala" "swift" "objective-c" "kotlin" "shell" "typescript")
 
-for l in ${allLanguages[@]}; do
-  echo $l
-  mix run -e "LowendinsightGet.GithubTrending.analyze(\"$l\")"
-done
+mix run --no-halt -e "LowendinsightGet.GithubTrending.process_languages()"

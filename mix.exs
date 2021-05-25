3b2aff0fac93da459aebfb51ff6e53264dae667f
kitplummer <kit.plummer@gtri.gatech.edu>
2021-05-25 13:21:58 -0400

    version to 0.6.5

--- mix.exs
+++ mix.exs
@@ -8,7 +8,7 @@ defmodule LowendinsightGet.MixProject do
   def project do
     [
       app: :lowendinsight_get,
-      version: "0.6.2",
+      version: "0.6.5",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
@@ -28,13 +28,13 @@ defmodule LowendinsightGet.MixProject do
       {:plug, "~> 1.6"},
       {:cowboy, "< 2.8.0", overide: true},
       {:plug_cowboy, "~> 2.0"},
-      {:credo, "~> 0.10", except: :prod, runtime: false},
+      {:credo, "~> 1.5", except: :prod, runtime: false},
       {:redix, ">= 0.0.0"},
       {:quantum, "~> 3.0-rc"},
       {:timex, "~> 3.0"},
       ## {:lowendinsight, path: "../lowendinsight"},
       ## {:lowendinsight, git: "git@bitbucket.org:kitplummer/lowendinsight", branch: "develop"}
-      {:lowendinsight, "0.6.2"},
+      {:lowendinsight, "0.6.5"},
       {:distillery, "~> 2.1"},
       {:excoveralls, "~> 0.10", only: :test}
     ]
